eacd1b58e08e0fc8487d3d41ced1691abd7f0c90
Amit Solankurkar <deadpool@192-168-1-11.tpgi.com.au>
2017-01-18 23:34:38 +1100

    Changes to accept parameter

--- src/main/java/au/amit/test/app/App.java
+++ src/main/java/au/amit/test/app/App.java
@@ -9,9 +9,13 @@ import au.amit.test.service.ScoreCalculator;
 public class App 
 {
     public static void main( String[] args ){
-        String input = "9 1 9 1";
-        ScoreCalculator calc = new ScoreCalculator();
-        int score = calc.calculate(input);
-        System.out.println(String.format("Score for \"%1$s\" is %2$s ", input, score) );
+    	if(args.length == 0) {
+    		System.out.println("Please provide input");
+    	} else {
+	        String input = args[0];
+	        ScoreCalculator calc = new ScoreCalculator();
+	        int score = calc.calculate(input);
+	        System.out.println(String.format("Score for \"%1$s\" is %2$s ", input, score) );
+    	}
     }
 }
